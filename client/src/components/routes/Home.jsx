2657935a060854e4ec30e20924a66cc5a50a82a1
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-01-10 14:36:54 +0530

    form section started

--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Hero from '../pages/home/hero/Hero';
 import Items from '../pages/home/product-page/Items';
 import Category from '../pages/home/category/Category';
+import Form from '../pages/home/sellerForm/Form';
+
 const Home = () => {
 	return (
 		<main>
 			<Hero />
             <Items />
-            <Category/>
+            <Category />
+            <Form />
 		</main>
 	);
 };
