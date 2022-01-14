e6a155dcd28b1f9e60d2a30100ed5b401859ddf5
Ayush Chugh <ayushchugh2006@gmail.com>
2022-01-14 20:12:09 +0530

    property upload form

--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Hero from '../pages/home/hero/Hero';
-import Items from '../pages/home/product-page/Items';
+import Items from '../pages/home/product/Items';
 import Category from '../pages/home/category/Category';
 import AddListing from '../pages/home/Listing/List';
 import Form from '../pages/home/form/Form';
-
 
 const Home = () => {
 	return (
 		<main>
 			<Hero />
-            <Items />
-            <Category />
-            <AddListing />
-            <Form />
+			<Items />
+			<Category />
+			<AddListing />
+			<Form />
 		</main>
 	);
 };
