d6b4516c9e771f271b8eeb9ebbfcf62115be509f
Ayush Chugh <ayushchugh2006@gmail.com>
2022-02-08 17:51:29 +0530

    search functionality

--- client/src/components/pages/properties/Properties.jsx
+++ client/src/components/pages/properties/Properties.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
 
 import { HSecondary, SSecondary } from '../../util/typography/Typography';
 import get from '../../../api/get';
@@ -13,78 +13,110 @@ import MapIcon from '@mui/icons-material/Map';
 import '../home/properties/properties.scss';
 
 const Properties = () => {
+	const navigate = useNavigate();
+	const [searchParams] = useSearchParams();
+	const searchQuery =
+		// this will check if search query is present only than convert it to lower case
+		searchParams.get('s') && searchParams.get('s').toLowerCase();
+
 	const [response, setResponse] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
+		setLoading(true);
 		get('/properties/all').then(data => {
-			setResponse(data.data);
+			// search functionality
+			// TODO: ask papa if to add more search options
+			if (searchQuery) {
+				const fileteredData = data.data.filter(
+					property =>
+						property.title.toLowerCase().includes(searchQuery) ||
+						property.description
+							.toLowerCase()
+							.includes(searchQuery) ||
+						property.address.toLowerCase().includes(searchQuery) ||
+						property.type.toLowerCase().includes(searchQuery) ||
+						property.catagory.toLowerCase().includes(searchQuery) ||
+						property.status.toLowerCase().includes(searchQuery) ||
+						property.price.includes(searchQuery) ||
+						property.specialPrice.includes(searchQuery) ||
+						property.size.includes(searchQuery)
+				);
+				// redirect to 404 page if no data found
+				if (fileteredData.length === 0) return navigate('/404');
+				setResponse(fileteredData);
+			} else {
+				setResponse(data.data);
+			}
 			setLoading(false);
 		});
-	}, []);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [searchQuery]);
 
 	return (
-		<section className="properties-section">
+		<>
 			{loading ? (
-				<Loader fullWidth />
+				<Loader fullScreen />
 			) : (
-				<div className="properties-section__properties">
-					{response.map(property => (
-						<Link
-							to={`/properties/${property._id}`}
-							key={property._id}
-						>
-							<div className="properties-section__property">
-								{/* Image */}
-								<div className="properties-section__property-image">
-									<img
-										src={property.images[0]?.url}
-										alt="property"
+				<section className="properties-section">
+					<div className="properties-section__properties">
+						{response.map(property => (
+							<Link
+								to={`/properties/${property._id}`}
+								key={property._id}
+							>
+								<div className="properties-section__property">
+									{/* Image */}
+									<div className="properties-section__property-image">
+										<img
+											src={property.images[0]?.url}
+											alt="property"
+										/>
+									</div>
+
+									{/* Mains */}
+									<HSecondary
+										title={property.title}
+										className="properties-section__property-title"
 									/>
-								</div>
 
-								{/* Mains */}
-								<HSecondary
-									title={property.title}
-									className="properties-section__property-title"
-								/>
+									<h4 className="properties-section__property-price">
+										₹ {property.price}
+									</h4>
 
-								<h4 className="properties-section__property-price">
-									₹ {property.price}
-								</h4>
+									<SSecondary
+										title={property.description}
+										className="properties-section__property-description"
+									/>
 
-								<SSecondary
-									title={property.description}
-									className="properties-section__property-description"
-								/>
-
-								{/* IconsBar */}
-								<div className="properties-section__property-iconbar">
-									<div className="properties-section__property-iconbar-icon">
-										<HotelIcon />
-										<h4>{property.bedroom}</h4>
-									</div>
-									<div className="properties-section__property-iconbar-icon">
-										<ShowerIcon />
-										<h4>{property.bathroom}</h4>
-									</div>
-									<div className="properties-section__property-iconbar-icon">
-										<CarRepairIcon />
-										<h4>{property.parking}</h4>
-									</div>
-									<div className="properties-section__property-iconbar-icon">
-										<MapIcon />
-										<h4>
-											{property.size} {property.unit}
-										</h4>
+									{/* IconsBar */}
+									<div className="properties-section__property-iconbar">
+										<div className="properties-section__property-iconbar-icon">
+											<HotelIcon />
+											<h4>{property.bedroom}</h4>
+										</div>
+										<div className="properties-section__property-iconbar-icon">
+											<ShowerIcon />
+											<h4>{property.bathroom}</h4>
+										</div>
+										<div className="properties-section__property-iconbar-icon">
+											<CarRepairIcon />
+											<h4>{property.parking}</h4>
+										</div>
+										<div className="properties-section__property-iconbar-icon">
+											<MapIcon />
+											<h4>
+												{property.size} {property.unit}
+											</h4>
+										</div>
 									</div>
 								</div>
-							</div>
-						</Link>
-					))}
-				</div>
+							</Link>
+						))}
+					</div>
+				</section>
 			)}
-		</section>
+		</>
 	);
 };
 
