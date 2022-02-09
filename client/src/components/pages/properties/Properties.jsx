e80361e3225b2a8f0646d19c4fcb8dbd0dda335a
Ayush Chugh <ayushchugh2006@gmail.com>
2022-02-09 15:31:33 +0530

    show 404 message if no property was found

--- client/src/components/pages/properties/Properties.jsx
+++ client/src/components/pages/properties/Properties.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Link, useSearchParams, useNavigate } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import { HSecondary, SSecondary } from '../../util/typography/Typography';
 import get from '../../../api/get';
 import Loader from '../../util/loader/Loader';
+import NotFound from '../notFound/NotFound';
 
 import HotelIcon from '@mui/icons-material/Hotel';
 import ShowerIcon from '@mui/icons-material/Shower';
@@ -13,7 +14,6 @@ import MapIcon from '@mui/icons-material/Map';
 import '../home/properties/properties.scss';
 
 const Properties = () => {
-	const navigate = useNavigate();
 	const [searchParams] = useSearchParams();
 	const searchQuery =
 		// this will check if search query is present only than convert it to lower case
@@ -21,6 +21,7 @@ const Properties = () => {
 
 	const [response, setResponse] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [notFound, setNotFound] = useState(false);
 
 	useEffect(() => {
 		setLoading(true);
@@ -42,8 +43,8 @@ const Properties = () => {
 						property.size.includes(searchQuery)
 				);
 				// redirect to 404 page if no data found
-				if (fileteredData.length === 0) return navigate('/404');
-				setResponse(fileteredData);
+				if (fileteredData.length === 0) setNotFound(true);
+				else setResponse(fileteredData);
 			} else {
 				setResponse(data.data);
 			}
@@ -58,61 +59,66 @@ const Properties = () => {
 				<Loader fullScreen />
 			) : (
 				<section className="properties-section">
-					<div className="properties-section__properties">
-						{response.map(property => (
-							<Link
-								to={`/properties/${property._id}`}
-								key={property._id}
-							>
-								<div className="properties-section__property">
-									{/* Image */}
-									<div className="properties-section__property-image">
-										<img
-											src={property.images[0]?.url}
-											alt="property"
+					{notFound ? (
+						<NotFound />
+					) : (
+						<div className="properties-section__properties">
+							{response.map(property => (
+								<Link
+									to={`/properties/${property._id}`}
+									key={property._id}
+								>
+									<div className="properties-section__property">
+										{/* Image */}
+										<div className="properties-section__property-image">
+											<img
+												src={property.images[0]?.url}
+												alt="property"
+											/>
+										</div>
+
+										{/* Mains */}
+										<HSecondary
+											title={property.title}
+											className="properties-section__property-title"
 										/>
-									</div>
 
-									{/* Mains */}
-									<HSecondary
-										title={property.title}
-										className="properties-section__property-title"
-									/>
+										<h4 className="properties-section__property-price">
+											₹ {property.price}
+										</h4>
 
-									<h4 className="properties-section__property-price">
-										₹ {property.price}
-									</h4>
+										<SSecondary
+											title={property.description}
+											className="properties-section__property-description"
+										/>
 
-									<SSecondary
-										title={property.description}
-										className="properties-section__property-description"
-									/>
-
-									{/* IconsBar */}
-									<div className="properties-section__property-iconbar">
-										<div className="properties-section__property-iconbar-icon">
-											<HotelIcon />
-											<h4>{property.bedroom}</h4>
-										</div>
-										<div className="properties-section__property-iconbar-icon">
-											<ShowerIcon />
-											<h4>{property.bathroom}</h4>
-										</div>
-										<div className="properties-section__property-iconbar-icon">
-											<CarRepairIcon />
-											<h4>{property.parking}</h4>
-										</div>
-										<div className="properties-section__property-iconbar-icon">
-											<MapIcon />
-											<h4>
-												{property.size} {property.unit}
-											</h4>
+										{/* IconsBar */}
+										<div className="properties-section__property-iconbar">
+											<div className="properties-section__property-iconbar-icon">
+												<HotelIcon />
+												<h4>{property.bedroom}</h4>
+											</div>
+											<div className="properties-section__property-iconbar-icon">
+												<ShowerIcon />
+												<h4>{property.bathroom}</h4>
+											</div>
+											<div className="properties-section__property-iconbar-icon">
+												<CarRepairIcon />
+												<h4>{property.parking}</h4>
+											</div>
+											<div className="properties-section__property-iconbar-icon">
+												<MapIcon />
+												<h4>
+													{property.size}{' '}
+													{property.unit}
+												</h4>
+											</div>
 										</div>
 									</div>
-								</div>
-							</Link>
-						))}
-					</div>
+								</Link>
+							))}
+						</div>
+					)}
 				</section>
 			)}
 		</>
