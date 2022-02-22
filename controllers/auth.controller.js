2fe7e66c71394aa81fe03c525317165d535c1981
Ayush Chugh <ayushchugh2006@gmail.com>
2022-02-22 14:18:03 +0530

    signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import User from '../models/user.model.js';
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare } from 'bcrypt';
 import { validationResult } from 'express-validator';
 import { generateJWT } from '../helpers/jwt.helper.js';
 import { httpOnlyCookie } from '../helpers/cookie.helper.js';
@@ -72,6 +72,64 @@ export const signup = async (req, res) => {
 		res.status(500).json({
 			success: false,
 			message: 'Internal Server Error',
+			data: {},
+		});
+	}
+};
+
+/* ---------------------------------- login --------------------------------- */
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		// validate body
+		const errors = validationResult(req).array();
+
+		if (errors.length > 0) {
+			return res.status(400).json({
+				success: false,
+				message: errors[0].msg,
+				data: {},
+			});
+		}
+
+		// check if user exists
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: 'User not found please check if your email is correct',
+				data: {},
+			});
+		}
+
+		// check if password is correct
+		const isPasswordCorrect = await compare(password, user.password);
+
+		if (!isPasswordCorrect) {
+			return res.status(401).json({
+				success: false,
+				message: 'Password is incorrect',
+				data: {},
+			});
+		}
+
+		// generate jwt token
+		const token = generateJWT({ id: user._id });
+
+		return res
+			.status(200)
+			.cookie(httpOnlyCookie('token', token, res))
+			.json({
+				success: true,
+				message: 'User logged in successfully',
+				data: user,
+			});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			message: 'Please check your email or password',
 			data: {},
 		});
 	}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,7 @@ import * as authController from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-const validationChecks = [
+const signupValidationChecks = [
 	check('name', 'This username must be at least 3+ character long')
 		.exists()
 		.isLength({ min: 3 }),
@@ -19,7 +19,12 @@ const validationChecks = [
 		.isStrongPassword(),
 ];
 
-authRouter.post('/auth/signup', validationChecks, authController.signup);
-// authRouter.post('/auth/login', authController.login);
+const loginValidationChecks = [
+	check('email', 'Please enter a valid email address').exists().isEmail(),
+	check('password', 'Please enter your password').exists(),
+];
+
+authRouter.post('/auth/signup', signupValidationChecks, authController.signup);
+authRouter.post('/auth/login', loginValidationChecks, authController.login);
 
 export default authRouter;
