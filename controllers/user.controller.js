e3d80afd4840af142d1359d8bc8e5d8166da6ee0
Ayush Chugh <ayushchugh2006@gmail.com>
2022-03-01 13:44:56 +0530

    get single user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,102 @@
 import { decodeJWT } from '../helpers/jwt.helper.js';
+import User from '../models/user.model.js';
+import logger from '../helpers/logger.helper.js';
 
+/* ------------------------- SECTION get single user ------------------------ */
+
+export const getSingleUser = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { listings, properties } = req.query;
+
+		// ANCHOR populate listings
+		if (listings === 'true' && properties === 'false') {
+			const user = await User.findById(id).populate('listings');
+
+			if (!user) {
+				return res.status(404).json({
+					success: false,
+					message: 'User not found',
+					data: {},
+				});
+			}
+
+			return res.status(200).json({
+				success: true,
+				message: 'User fetched successfully',
+				data: user,
+			});
+		}
+
+		// ANCHOR populate properties
+		if (listings === 'false' && properties === 'true') {
+			const user = await User.findById(id).populate('properties');
+
+			if (!user) {
+				return res.status(404).json({
+					success: false,
+					message: 'User not found',
+					data: {},
+				});
+			}
+
+			return res.status(200).json({
+				success: true,
+				message: 'User fetched successfully',
+				data: user,
+			});
+		}
+
+		// ANCHOR populate both listings and properties
+		if (listings === 'true' && properties === 'true') {
+			const user = await User.findById(id)
+				.populate('listings')
+				.populate('properties');
+
+			if (!user) {
+				return res.status(404).json({
+					success: false,
+					message: 'User not found',
+					data: {},
+				});
+			}
+
+			return res.status(200).json({
+				success: true,
+				message: 'User fetched successfully',
+				data: user,
+			});
+		}
+
+		// ANCHOR populate neither listings nor properties
+		const user = await User.findById(id);
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: 'User not found',
+				data: {},
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			message: 'User fetched successfully',
+			data: user,
+		});
+	} catch (err) {
+		logger.error(err);
+		res.status(500).json({
+			success: false,
+			message: 'Internal server error',
+			data: {},
+		});
+	}
+};
+
+/* -------------------------- !SECTION get single user end -------------------------- */
+
+/* --------------------------------- SECTION decode --------------------------------- */
 export const decode = async (req, res) => {
 	const id = await decodeJWT(req.cookies.token);
 
@@ -9,3 +106,5 @@ export const decode = async (req, res) => {
 		data: id,
 	});
 };
+
+/* ----------------------------- !SECTION decode ---------------------------- */
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ import * as userController from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
+userRouter.get('/users/single/:id', userController.getSingleUser);
 userRouter.get('/users/decode', userController.decode);
 
 export default userRouter;
