4cfcc2b9246c6f7ccab44a06e61eab3e5b068c00
Ayush Chugh <ayushchugh2006@gmail.com>
2022-07-13 13:04:05 +0530

    removed client from this repo and put into new repo https://github.com/shriproperty/client

--- client/src/helpers/date.ts
+++
@@ -1,9 +0,0 @@
-export const dateToIntlDateFormatter = (date: Date | string) => {
-	const dateFromString = new Date(date);
-
-	const formattedDate = new Intl.DateTimeFormat(navigator.language).format(
-		dateFromString
-	);
-
-	return formattedDate;
-};
